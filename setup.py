f678d2570134ef417ea5ed75322d33de8629fd2d
Marcelo Castellano <marce.geek22@gmail.com>
2018-10-29 01:39:38 -0300

    probando deploy

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="personal_inventory",
+    version="0.0.1",
+    author="Marcelo Castellano",
+    author_email="marce.geek22@gmail.com",
+    description="Web application for managing an inventory o personal items",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcegeek/personal-inventory",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'sqlalchemy',
+        'Flask',
+        'email_validator',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
