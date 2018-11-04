4aae7c646cd46c0e5023f47b6fccab763cb4f624
Marcelo Castellano <marce.geek22@gmail.com>
2018-11-04 19:26:11 -0300

    fix dirección de bind en producción

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import config
 from personal_inventory.presentation import app
 
 if config.ENVIRONMENT == config.Environment.PRODUCTION:
-    subprocess.call('gunicorn personal_inventory.presentation:app', shell=True)
+    subprocess.call('gunicorn -b 0.0.0.0 personal_inventory.presentation:app', shell=True)
 else:
     app.run()
