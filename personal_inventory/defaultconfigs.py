de54dcd3f5c5290672f05ca83107c5b0ac5d684a
Marcelo Castellano <marce.geek22@gmail.com>
2018-11-03 21:26:42 -0300

    fix sqlite multihilo

--- personal_inventory/defaultconfigs.py
+++ personal_inventory/defaultconfigs.py
@@ -1,9 +1,15 @@
 class DataConfig:
-    DB_CONF = {'sqlalchemy.url': 'sqlite://'}
+    DB_CONF = {
+        'sqlalchemy.url': 'sqlite://',
+        'sqlalchemy.connect_args': {'check_same_thread': False}
+    }
 
 
 class PersistentDataConfig(DataConfig):
-    DB_CONF = {'sqlalchemy.url': 'sqlite:///db.sqlite'}
+    DB_CONF = {
+        'sqlalchemy.url': 'sqlite:///db.sqlite',
+        'sqlalchemy.connect_args': {'check_same_thread': False}
+    }
 
 
 class MemoryDataConfig(DataConfig):
