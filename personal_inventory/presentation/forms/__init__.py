8317f418b2a5fab04ddc3bbcf429c07bc612b765
Marcelo Castellano <marce.geek22@gmail.com>
2018-12-13 21:16:59 -0300

    limpieza de espacios en blanco extra en formularios

--- personal_inventory/presentation/forms/__init__.py
+++ personal_inventory/presentation/forms/__init__.py
@@ -1,8 +1,20 @@
-from wtforms import Form, StringField, PasswordField, SelectField, validators
+from wtforms import Form
 
 
 class BaseForm(Form):
+    class Meta:
+        @staticmethod
+        def bind_field(form, unbound_field, options):
+            filters = unbound_field.kwargs.get('filters', [])
+            filters.append(_strip_filter)
+            return unbound_field.bind(form=form, filters=filters, **options)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.global_errors = []
+
+
+def _strip_filter(value):
+    if value is not None and hasattr(value, 'strip'):
+        return value.strip()
+    return value
