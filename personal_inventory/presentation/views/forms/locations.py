08ce6f3a4e6df38fed79d895698d4c51eeb66525
Marcelo Castellano <marce.geek22@gmail.com>
2019-02-14 14:55:43 -0300

    fix eliminar ubicación

--- personal_inventory/presentation/views/forms/locations.py
+++ personal_inventory/presentation/views/forms/locations.py
@@ -43,6 +43,5 @@ class LocationDeleteForm(DeleteForm):
         if location is not None:
             self.title = _('Delete location')
             self.modal_id = 'delete-location-{}-modal'.format(location.id)
-            self.action = fl.url_for('item_delete', item_id=location.id)
+            self.action = fl.url_for('location_delete', location_id=location.id)
             self.extra_body_html = self.delete_body(location.description)
-            self.extra_footer_html = '<p>Jeje esto es raro</p>'
