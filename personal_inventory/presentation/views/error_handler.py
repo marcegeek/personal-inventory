7c5e87d9d5487b04943c11259673b7556cf9e932
Marcelo Castellano <marce.geek22@gmail.com>
2019-02-28 06:35:23 -0300

    agrego más traducciones

--- personal_inventory/presentation/views/error_handler.py
+++ personal_inventory/presentation/views/error_handler.py
@@ -1,7 +1,7 @@
 from flask_babel import gettext as _
 
 from personal_inventory.business.logic import RequiredFieldError, DeleteForeingKeyError, InvalidLength
-from personal_inventory.business.logic.item_logic import InvalidValue
+from personal_inventory.business.logic.item_logic import InvalidValue, RepeatedItemNameError
 from personal_inventory.business.logic.location_logic import RepeatedLocationNameError
 from personal_inventory.business.logic.user_logic import InvalidEmailError, RepeatedUsernameError, InvalidUsernameError
 
@@ -23,6 +23,8 @@ def error_str(error):
         return _('Username must be made of lowercase letters, digits and underscores')
     elif isinstance(error, RepeatedLocationNameError):
         return _('Location name repeated')
+    elif isinstance(error, RepeatedItemNameError):
+        return _('Item name repeated')
     elif isinstance(error, InvalidValue):
         return _('Invalid value')
     else:
